46033f0513cf8831ede125b6e6fe1f266e4a2b36
Gabriel <gaboico@gmail.com>
2023-03-21 14:13:54 -0400

    Creating the first test

--- UT_NextPemut/UT_NextPemut/CNxtPermut.cs
+++ UT_NextPemut/UT_NextPemut/CNxtPermut.cs
@@ -1,9 +1,21 @@
-﻿namespace UT_NextPemut
+﻿using System;
+
+namespace UT_NextPemut
 {
     internal class CNxtPermut
     {
         public CNxtPermut()
         {
+        }
+
+        internal object get_Vector()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal object get_Vector(int[] pvector)
+        {
+            return 0;
         }
     }
 }
--- UT_NextPemut/UT_NextPemut/UnitTest1.cs
+++ UT_NextPemut/UT_NextPemut/UnitTest1.cs
@@ -4,11 +4,16 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace UT_NextPemut
 {
     [TestClass]
-    public class UnitTest1
+    public class UT_NxtPermut
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Return0_Recieve_Empty()
         {
-        }
+            var permutation = new CNxtPermut();
+            int[] pvector= new int[3];
+            var result = permutation.get_Vector(pvector);
+            Assert.AreEqual(0, result);
+
+;        }
     }
 }
